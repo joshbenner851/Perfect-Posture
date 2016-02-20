7af74c7614b230ed5f3450cd3ccd3cfc8c0da67e
Joel <joelw16@gmail.com>
2016-02-21 03:07:31 +0800

    Chart.js added to profile.

--- Website/m2xAPI.js
+++ Website/m2xAPI.js
@@ -25,10 +25,29 @@ window.onload = function() {
         tiltData = JSON.parse(xhr.response).values;
         console.log(tiltData);
 
-        for(var i=0; i <tiltData.length && tiltData[i].value != null; i++){
-            console.log(tiltData[i].value);
-        }
+
+
+            var labels = [],data=[];
+            for(var i=0; i <30 && tiltData[i].value != null; i++){
+                labels.push(tiltData[i].timestamp);
+                data.push(tiltData[i].value);
+            }
+
+            var tempData = {
+                labels : labels,
+                datasets : [{
+                    fillColor : "rgba(172,194,132,0.4)",
+                    strokeColor : "#ACC26D",
+                    pointColor : "#fff",
+                    pointStrokeColor : "#9DB86D",
+                    data : data
+                }]
+            };
+
+            var temp = document.getElementById('canvas').getContext('2d');
+            var lineChart = new Chart(temp).Line(tempData);
     });
 
 };
+
 
